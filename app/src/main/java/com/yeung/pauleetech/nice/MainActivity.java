385b95ca7f8a4f2a150975fc8b082135919a5416
yongthurio <2271210749@qq.com>
2018-05-05 16:50:26 +0800

    5.5日提交

--- app/src/main/java/com/yeung/pauleetech/nice/MainActivity.java
+++ app/src/main/java/com/yeung/pauleetech/nice/MainActivity.java
@@ -8,21 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.yeung.pauleetech.nice.Fragment.HomeFragment;
-import com.yeung.pauleetech.nice.Fragment.RecommendFragment;
+import com.yeung.pauleetech.nice.MainPageLayout.Fragment.HomeFragment;
+import com.yeung.pauleetech.nice.MeLayout.MeFragement;
+import com.yeung.pauleetech.nice.ShoppingCartPageLayout.ShoppingCartFragment;
 
-/*import com.yeung.pauleetech.nice.RollviewpagerImt.NetImageActivity;*/
+/*import com.yeung.pauleetech.nice.MainPageLayout.Fragment.RollviewpagerImt.NetImageActivity;*/
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private ImageView imageView_home;
-    private ImageView imageView_recommend;
-    private ImageView imageView_discovery;
-    private ImageView imageView_me;
     private LinearLayout home;
     private LinearLayout recommend;
     private LinearLayout discovery;
@@ -32,13 +28,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView textView_discovery;
     private TextView textView_me;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         replaceFragment(new HomeFragment());
         home=(LinearLayout)findViewById(R.id.apphome);
-        recommend =(LinearLayout)findViewById(R.id.recommend);
+        recommend =(LinearLayout)findViewById(R.id.shoppingcart);
         discovery=(LinearLayout)findViewById(R.id.discovery);
         me=(LinearLayout)findViewById(R.id.me);
         textView_home=(TextView)findViewById(R.id.text_home);
@@ -58,13 +55,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager=getSupportFragmentManager();
         FragmentTransaction transaction=fragmentManager.beginTransaction();
+        //FragmentTransaction是对Fragment进行添加，替换，移除等操作的。在使用add(),replace(),remove()时可以动态的给每一个Fragment添加一个标签，
+        // 下次方便FragmentManager通过tag标签进行查找。最后记得ft.commit();
         transaction.replace(R.id.top_layout,fragment);
         transaction.commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar,menu);
+        getMenuInflater().inflate(R.menu.toolbar,menu);  //toolbar干什么的？
         return true;
     }
 
@@ -78,13 +77,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 textView_discovery.setTextColor(Color.BLACK);
                 textView_me.setTextColor(Color.BLACK);
                 break;
-            case R.id.recommend:
-                replaceFragment(new RecommendFragment());
-                textView_home.setTextColor(Color.BLACK);
-                textView_recommend.setTextColor(Color.GREEN);
-                textView_discovery.setTextColor(Color.BLACK);
-                textView_me.setTextColor(Color.BLACK);
-                break;
             case R.id.discovery:
                 replaceFragment(new HomeFragment());
                 textView_home.setTextColor(Color.BLACK);
@@ -92,8 +84,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 textView_discovery.setTextColor(Color.GREEN);
                 textView_me.setTextColor(Color.BLACK);
                 break;
+            case R.id.shoppingcart:
+                replaceFragment(new ShoppingCartFragment());
+                textView_home.setTextColor(Color.BLACK);
+                textView_recommend.setTextColor(Color.GREEN);
+                textView_discovery.setTextColor(Color.BLACK);
+                textView_me.setTextColor(Color.BLACK);
+                break;
             case R.id.me:
-                replaceFragment(new HomeFragment());
+                replaceFragment(new MeFragement());
                 textView_home.setTextColor(Color.BLACK);
                 textView_recommend.setTextColor(Color.BLACK);
                 textView_discovery.setTextColor(Color.BLACK);
@@ -102,12 +101,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    /*public void simple(View view){       //在主活动中可以直接这样写？函数名==控件名绑定监听器？在布局中写上onClick="函数名"
-        Intent i = new Intent(this,SimpleActivity.class);
-        startActivity(i);
-    }*/
-
-    /*public void loop(View view){
+    /*public void loop(View view){      //在主活动中可以直接这样写？函数名==控件名绑定监听器？在布局中写上onClick="函数名"
         Intent i = new Intent(this,LoopActivity.class);
         startActivity(i);
     }*/
@@ -116,6 +110,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent i = new Intent(this,NetImageActivity.class);
         startActivity(i);
     }*/
-
-
 }
